754e3c408250af407af70ef5e955e8d8c23bbbb3
Souvik Biswas <sbis1999@gmail.com>
2020-11-20 19:08:42 +0530

    Hide the debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         fontFamily: 'Goldman',
       ),
+      debugShowCheckedModeBanner: false,
       home: LoginPage(),
     );
   }
