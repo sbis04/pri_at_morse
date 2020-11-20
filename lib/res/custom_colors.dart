46d789a30dadf771c1181139c697845636a7bad7
Souvik Biswas <sbis1999@gmail.com>
2020-11-20 19:16:55 +0530

    Added custom colors

---
+++ lib/res/custom_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static final Color highlight = Color(0xFF00e0c6);
+  static final Color medium = Color(0xFF019393);
+  static final Color dark = Color(0xFF026171);
+  static final Color background = Color(0xFF2a3950);
+}
